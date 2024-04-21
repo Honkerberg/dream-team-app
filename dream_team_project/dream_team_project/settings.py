08d1d1922fb66133eb61fb2b7976fa596223496e
Martin Ošťádal <ostadalmartin96@gmail.com>
2024-04-21 19:48:14 +0200

    refactor(settings): :recycle: warehouse app refactored

--- dream_team_project/dream_team_project/settings.py
+++ dream_team_project/dream_team_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "warehouse",  # main app
+    "warehouse.apps.WarehouseConfig",  # main app
 ]
 
 MIDDLEWARE = [
