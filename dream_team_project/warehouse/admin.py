f4e143ed87f06ae4b08caba81b384ee11c97a4e1
Martin Ošťádal <ostadalmartin96@gmail.com>
2024-04-23 08:23:30 +0200

    feat(warehouse-admin): :sparkles: Admin models init + check correct person assignment
    Initialization of models in admin, validation for assigning correct person

--- dream_team_project/warehouse/admin.py
+++ dream_team_project/warehouse/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from warehouse.models import Customer, Employee, Person
 
-# Register your models here.
+
+class CustomerAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change) -> None:
+        if obj.person_id.typ_osoba == "CUS":
+            return super().save_model(request, obj, form, change)
+        else:
+            raise ValueError
+
+
+admin.site.register(Customer, CustomerAdmin)
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change) -> None:
+        if obj.person_id.typ_osoba == "EMP":
+            return super().save_model(request, obj, form, change)
+        else:
+            raise ValueError
+
+
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Person)
