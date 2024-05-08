25f439d582e977f25eed795844323c375e6c623f
Martin Ošťádal <ostadalmartin96@gmail.com>
2024-05-08 12:31:02 +0200

    refactor(warehouse-admin): :recycle: Admin objects extended with list_display and ordering

--- dream_team_project/warehouse/admin.py
+++ dream_team_project/warehouse/admin.py
@@ -4,6 +4,8 @@ from warehouse.exceptions import WrongPersonException
 
 
 class CustomerAdmin(admin.ModelAdmin):
+    list_display = ["person_id", "email", "ship_address"]
+    ordering = ["person_id"]
     def save_model(self, request, obj, form, change) -> None:
         if obj.person_id.typ_osoba == "CUS":
             return super().save_model(request, obj, form, change)
@@ -17,6 +19,8 @@ admin.site.register(Customer, CustomerAdmin)
 
 
 class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ["person_id", "job_position"]
+    ordering = ["person_id"]
     def save_model(self, request, obj, form, change) -> None:
         if obj.person_id.typ_osoba == "EMP":
             return super().save_model(request, obj, form, change)
@@ -27,4 +31,9 @@ class EmployeeAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Employee, EmployeeAdmin)
-admin.site.register(Person)
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ["id", "first_name", "last_name", "birth_date"]
+    ordering = ["id"]
+
+admin.site.register(Person, PersonAdmin)
