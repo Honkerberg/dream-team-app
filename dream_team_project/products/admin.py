151e1488d5ebb9a6780b0bd6822d11c7bfed4820
Martin Ošťádal <ostadalmartin96@gmail.com>
2024-05-08 13:08:37 +0200

    feat(products-admin): :sparkles: Models register
    To see models and insert data, these models are registered on admin site.

--- dream_team_project/products/admin.py
+++ dream_team_project/products/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from products.models import (
+    Category,
+    Manufacturer,
+    Product,
+    StockInventory,
+    StockPosition,
+)
+
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Manufacturer)
+admin.site.register(Product)
+admin.site.register(StockInventory)
+admin.site.register(StockPosition)
