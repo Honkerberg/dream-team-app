d1ac68b16169bbb27eab801f472575dbc583891c
Martin Ošťádal <ostadalmartin96@gmail.com>
2024-05-19 16:51:30 +0200

    feat: :sparkles: Classes add to show data in more detail

--- dream_team_project/orders/admin.py
+++ dream_team_project/orders/admin.py
@@ -2,7 +2,27 @@ from django.contrib import admin
 
 from orders.models import Order, OrderLines, Shipping
 
+
 # Register your models here.
-admin.site.register(Order)
-admin.site.register(Shipping)
-admin.site.register(OrderLines)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "customer_id", "shipment_id", "employee_id"]
+    ordering = ["customer_id"]
+
+
+admin.site.register(Order, OrderAdmin)
+
+
+class OrderLinesAdmin(admin.ModelAdmin):
+    list_display = ["id", "order_id", "product_id", "quantity"]
+    ordering = ["id"]
+
+
+admin.site.register(OrderLines, OrderLinesAdmin)
+
+
+class ShippingAdmin(admin.ModelAdmin):
+    list_display = ["shipment_name", "shipment_price"]
+    ordering = ["shipment_name"]
+
+
+admin.site.register(Shipping, ShippingAdmin)
--- dream_team_project/products/admin.py
+++ dream_team_project/products/admin.py
@@ -10,8 +10,21 @@ from products.models import (
 
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Manufacturer)
-admin.site.register(Product)
-admin.site.register(StockInventory)
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = ["manufacturer_name", "manufacturer_country"]
+admin.site.register(Manufacturer, ManufacturerAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["category_name", "category_description", "stock_position_id"]
+admin.site.register(Category, CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["product_name", "product_price", "product_currency", "product_manufacturer_id"]
+admin.site.register(Product, ProductAdmin)
+
+
+class StockInventoryAdmin(admin.ModelAdmin):
+    list_display = ["product_id", "quantity", "date_of_acceptance", "date_of_manufacture", "expiration_date"]
+admin.site.register(StockInventory, StockInventoryAdmin)
+
 admin.site.register(StockPosition)
